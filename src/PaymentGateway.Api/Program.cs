bcb6ca7d8b229d6f2e1f10cab564bedfd8227e14
achrefbrigui <62870799+achrefbrigui@users.noreply.github.com>
2026-09-19 18:11:12 +0200

    feat : services di

--- src/PaymentGateway.Api/Program.cs
+++ src/PaymentGateway.Api/Program.cs
@@ -1,4 +1,9 @@
+using FluentValidation;
+
+using PaymentGateway.Api.Clients.Bank;
+using PaymentGateway.Api.Models.Requests;
 using PaymentGateway.Api.Services;
+using PaymentGateway.Api.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +15,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IPaymentRepository, PaymentsRepository>();
+builder.Services.AddHttpClient<IBankClient, BankClient>()
+    .ConfigureHttpClient(cfg =>
+    {
+        cfg.BaseAddress = new Uri("http://localhost:8080/");
+    });
+builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.AddScoped<IValidator<PostPaymentRequest>, PostPaymentRequestValidator>();
 
 var app = builder.Build();
 
@@ -27,3 +39,8 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program()
+{
+
+}
