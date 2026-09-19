fe9de6869652b70e05e90eb1f0dfe3374d6277ef
achrefbrigui <62870799+achrefbrigui@users.noreply.github.com>
2026-09-19 17:29:00 +0200

    refactor : fix cvv type

--- src/PaymentGateway.Api/Models/Requests/PostPaymentRequest.cs
+++ src/PaymentGateway.Api/Models/Requests/PostPaymentRequest.cs
@@ -10,5 +10,5 @@ public class PostPaymentRequest
     public string ExpiryDate { get; set; }
     public string Currency { get; set; }
     public int Amount { get; set; }
-    public int Cvv { get; set; }
+    public string Cvv { get; set; }
 }
