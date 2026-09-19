bdb45e623c81818b055e32b2541170ab658ee89a
achrefbrigui <62870799+achrefbrigui@users.noreply.github.com>
2026-09-19 17:52:20 +0200

    feat: integrate bank simulator client

---
+++ src/PaymentGateway.Api/Models/Responses/BankAuthorizationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace PaymentGateway.Api.Models.Responses
+{
+    public class BankAuthorizationResponse
+    {
+        public bool Authorized { get; set; }
+        public string? AuthorizationCode { get; set; }
+    }
+}
--- src/PaymentGateway.Api/Services/PaymentService.cs
+++ src/PaymentGateway.Api/Services/PaymentService.cs
@@ -1,4 +1,5 @@
-﻿using PaymentGateway.Api.Mappers;
+﻿using PaymentGateway.Api.Clients.Bank;
+using PaymentGateway.Api.Mappers;
 using PaymentGateway.Api.Models.Requests;
 using PaymentGateway.Api.Models.Responses;
 
@@ -6,10 +7,12 @@ namespace PaymentGateway.Api.Services
 {
     public class PaymentService : IPaymentService
     {
+        private readonly IBankClient _bankClient;
         private readonly IPaymentRepository _paymentRepository;
 
-        public PaymentService( IPaymentRepository paymentRepository)
+        public PaymentService(IBankClient bankClient, IPaymentRepository paymentRepository)
         {
+            _bankClient = bankClient;
             _paymentRepository = paymentRepository;
         }
 
@@ -17,7 +20,15 @@ namespace PaymentGateway.Api.Services
         {
             var payment = request.ToDomain();
 
-            //// TODO : implement bank client
+            var bankResponse = await _bankClient.Authorize(request);
+            if (bankResponse?.Authorized == true && !string.IsNullOrEmpty(bankResponse?.AuthorizationCode))
+            {
+                payment.Authorize(bankResponse.AuthorizationCode);
+            }
+            else
+            {
+                payment.Decline();
+            }
 
             _paymentRepository.Add(payment);
 
